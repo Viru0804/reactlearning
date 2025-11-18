0183109fed6655d3b56e7cb94b7dd7d1befa72ce
Viru0804 <dsaviru6@gmail.com>
2025-11-18 23:29:38 +0530

    Homepage

--- 07reactRouter/src/components/Home/home.jsx
+++ 07reactRouter/src/components/Home/home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                         <h2 className="text-4xl font-bold sm:text-5xl">
-                            Download Now
+                            Download NOW
                             <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
                         </h2>
 
@@ -30,7 +30,7 @@ export default function Home() {
                                 />
                             </svg>
 
-                            &nbsp; Download now
+                            &nbsp; Download NOW
                         </Link>
                     </div>
                 </div>
@@ -44,7 +44,7 @@ export default function Home() {
                 <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
             </div>
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+            <h1 className="text-center text-3xl sm:text-5xl py-10 font-medium">Lorem Ipsum YOJO</h1>
         </div>
     );
 }
